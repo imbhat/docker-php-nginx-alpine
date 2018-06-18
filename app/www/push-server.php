5738aad77c3c3431b0c7c32faf9721e653a06a92
Sagar Vaidya <imsagar@gmail.com>
2018-06-18 18:47:48 +0530

    update

--- app/www/push-server.php
+++ app/www/push-server.php
@@ -4,7 +4,7 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use PushNotification\Pusher;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . 'init.php';
 
 $server = IoServer::factory(
         new HttpServer(
--- app/www/push-to-server.php
+++ app/www/push-to-server.php
@@ -1,5 +1,5 @@
 <?php
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . 'init.php';
 
 $loop   = React\EventLoop\Factory::create();
 $pusher = new PushNotification\Pusher;
